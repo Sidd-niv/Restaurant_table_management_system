91e869ffc74cd5d2a0fea0b5b2093eb043e34e2e
Siddhant Nivilkar <siddhantnivilkar333@gmail.com>
2022-04-01 16:28:48 +0530

    Modules completed

--- database_con.py
+++ database_con.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import psycopg2
 
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Sidd@localhost/Restaurant_Table_Management_System_DB"
 
