5cf76c6273c08c61a499076a9725d6adef51841b
Siddhant Nivilkar <siddhantnivilkar333@gmail.com>
2022-04-05 13:09:56 +0530

    deploy

--- database_con.py
+++ database_con.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Sidd@localhost/Restaurant_Table_Management_System_DB"
+# SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:Sidd@localhost/Restaurant_Table_Management_System_DB"
 
 # Database driver url
-# SQLALCHEMY_DATABASE_URL = "postgresql://aujdjwwviinzbk:a96c3930e1955458823c071e6a7fe937f9b9e0bab51ded4cc14240bd4dcb2ebb@ec2-44-194-4-127.compute-1.amazonaws.com:5432/da17jrcbfpjgub"
+SQLALCHEMY_DATABASE_URL = "postgresql://aujdjwwviinzbk:a96c3930e1955458823c071e6a7fe937f9b9e0bab51ded4cc14240bd4dcb2ebb@ec2-44-194-4-127.compute-1.amazonaws.com:5432/da17jrcbfpjgub"
 
 # Connecting database
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
